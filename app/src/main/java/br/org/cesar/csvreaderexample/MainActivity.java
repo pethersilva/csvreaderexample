2c014da3ba747d48774cac2fba2d0f6bd2d7610e
Pether Silva <petherjs@motorola.com>
2019-10-17 10:49:23 -0300

    adding csv square reader implemetation

--- app/src/main/java/br/org/cesar/csvreaderexample/MainActivity.java
+++ app/src/main/java/br/org/cesar/csvreaderexample/MainActivity.java
@@ -1,13 +1,20 @@
 package br.org.cesar.csvreaderexample;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
+
+import br.org.cesar.csvreaderexample.model.Square;
+import br.org.cesar.csvreaderexample.service.SquareService;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		SquareService squareService = new SquareService(this);
+		squareService.getSquare();
+	}
 }
